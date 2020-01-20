89611b79e3824cbfbcb6f6edeaff6754570c8d62
Enzo Peña <59707399+enzohachi@users.noreply.github.com>
2020-01-20 13:07:46 -0300

    Update Patronz.java

--- Patronz.java
+++ Patronz.java
@@ -4,7 +4,7 @@
 package patronz;
 
 /**
- * @author Nonita
+ * @author enzohachi
  *
  */
 import java.util.Scanner;
@@ -19,9 +19,19 @@ public class Patronz {
 	    Scanner input = new Scanner(System.in);
 	    System.out.print("Ingrese numero: ");
 	    int n = input.nextInt();
+//sirve para hacer la matriz con un ciclo anidado
 	    for (int fila = 0; fila < n; fila++) {
 	        for (int columna = 0; columna < n; columna++) {
-	            if (fila == 0 || fila == n - 1 || columna == n - 1 - fila) {
+
+			
+// fila ==0 es para poscionarse en el extremo superior de la matriz
+//ó
+// columna = n -1 es para posicionarse en el extremo inferior de la matriz
+//ó
+// columna == n -1 - fila es para posicionarse en forma de backslash o slash invertido y asi poder formar la "Z"
+			// tambien podria ser fila ==n-1-columna y daria el mismo resultado
+	            
+			if (fila == 0 || fila == n - 1 || columna == n - 1 - fila) {
 	                System.out.print("*");
 	            } else {
 	                System.out.print(" ");
