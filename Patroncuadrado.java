1648ef8bd10a0c25a067287bf5199ab66417dc07
Enzo Peña <59707399+enzohachi@users.noreply.github.com>
2020-01-20 12:59:47 -0300

    Update Patroncuadrado.java

--- Patroncuadrado.java
+++ Patroncuadrado.java
@@ -19,8 +19,17 @@ public class Patroncuadrado {
 Scanner input = new Scanner(System.in);
 System.out.print("Ingrese numero: ");
 int n = input.nextInt();
+		
+//ambos for son para generar una matriz por medio del ciclo anidado		
 for (int fila = 0; fila < n; fila++) {
     for (int columna = 0; columna < n; columna++) {
+	    
+//fila ==0 es para que mas abajo pueda posicionarse en todas las filas que esten en posicion 0, osea dibujar una linea vertical en el lado izquierdo
+//ó
+//fila == n-1 es para que se posicione en el extremo derecho y dibujar la linea vertical 
+//columna==0 es para posicionarse la linea superior horizontal
+//columna==n-1 es para posicionarse en la linea inferior horizonal
+	    
         if (fila == 0 || fila == n - 1 || columna==0|| columna==n-1) {
             System.out.print("*");
         } else {
