0d87f4e9c6ff374cb4350a93b54889cd01242026
Enzo Peña <59707399+enzohachi@users.noreply.github.com>
2020-01-21 11:01:54 -0300

    Update Patronx1.java

--- Patronx1.java
+++ Patronx1.java
@@ -25,7 +25,7 @@ public class Patronx1 {
 		    for(int columna=0; columna<num; columna++){
 			    
 //fila=columna es para cuando existe igualdad de numero de fila con columna, esto es para poder dibujar el backslash (o slash invertido) para poder esa linea de la forma de X
-//fila+columna=num-1 es para igualar la suma de fila con columna con la cantidad del imput impuesto menos 1, porque se cuenta desde el cero, de esta forma se obtiene el otro slach para poder imprimir ambos mas bajo  
+//fila+columna=num-1 es para igualar la suma de fila con columna con la cantidad del imput impuesto menos 1, porque se cuenta desde el cero, de esta forma se obtiene el otro slash para poder imprimir ambos mas bajo  
 		        if(fila==columna  || (fila+columna)==num-1){
 		            System.out.print("x");
 		        }else{
